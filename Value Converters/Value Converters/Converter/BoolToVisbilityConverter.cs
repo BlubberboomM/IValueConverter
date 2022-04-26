97d050ec9a2c40c002355acbfc748b7b4d9428e6
BlubberboomM <77407933+BlubberboomM@users.noreply.github.com>
2022-04-25 21:35:12 -0400

    Add files via upload

---
+++ Value Converters/Value Converters/Converter/BoolToVisbilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Value_Converters.Converter
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var BooleanVal = (bool)value;
+            if (BooleanVal)
+                return Visibility.Visible;
+            else
+                return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
